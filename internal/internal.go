d21a1784d8d48df54c9f4d535377551c803019eb
teavver <jerzyx11@gmail.com>
2023-09-07 11:39:06 +0200

    chore: update internal setup

--- internal/internal.go
+++ internal/internal.go
@@ -2,7 +2,6 @@ package internal
 
 import (
 	"context"
-	"flag"
 	"fmt"
 	"os"
 
@@ -14,30 +13,33 @@ import (
 	"github.com/joho/godotenv"
 )
 
-var nodeConfig = &node.NodeConfig{}
-
-var portFlag = flag.String("port", "1337", "Listening port (default 1337)")
-var idFlag = flag.Int("id", 0, "Player id")
+var nodeConfig node.NodeConfig
 
 func Main(ctx context.Context, pc chan player.Player, cc chan types.ChatMessage, isRelay bool) {
 
 	// Load env
 	err := godotenv.Load()
 	if err != nil {
-		panic("Error loading .env file")
+		panic("[ERR] couldn't load .env file")
 	}
 
+	// Network conf
+	port := os.Getenv("PORT")
 	relayIp := os.Getenv("RELAY_IP")
 	relayAddr := os.Getenv("RELAY_ADDR")
-	relayPort := "1337"
-	nodeConfig.RelayAddr = relayAddr
-	nodeConfig.RelayPort = relayPort
-	nodeConfig.RelayIp = relayIp
-	nodeConfig.Port = *portFlag
+	relayPort := os.Getenv("RELAY_PORT")
 
-	// TODO:
-	// Check if all .env vars are != nil based on flags
-	// to prevent 10-line errs from libp2p
+	// Check if conf is OK
+	if port == "" || relayIp == "" || relayAddr == "" || relayPort == "" {
+		panic("[ERR] .env config is incomplete")
+	}
+
+	nodeConfig = node.NodeConfig{
+		RelayAddr: relayAddr,
+		RelayPort: relayPort,
+		RelayIp:   relayIp,
+		Port:      port,
+	}
 
 	if isRelay {
 		runStandaloneGameNode(ctx)
@@ -47,15 +49,15 @@ func Main(ctx context.Context, pc chan player.Player, cc chan types.ChatMessage,
 }
 
 func runPlayerNode(pc chan player.Player, cc chan types.ChatMessage, ctx context.Context) {
-	p := player.New(ctx, *idFlag)
-	p.Init(ctx, cc, false, nodeConfig)
-	fmt.Printf("Player id: %d connected!\n", *idFlag)
+	p := player.New(ctx, 0)
+	p.Init(ctx, cc, false, &nodeConfig)
+	fmt.Printf("Player id: %d connected!\n", 0)
 	pc <- p
 }
 
 func runStandaloneGameNode(ctx context.Context) {
 	gn := gamenode.New()
-	gn.Init(ctx, true, nodeConfig)
+	gn.Init(ctx, true, &nodeConfig)
 	fmt.Println("===============================================================")
 	fmt.Println("Relay ID: ", gn.NetworkNode.Host.GetPeerInfo().ID.String())
 	fmt.Println("===============================================================")
